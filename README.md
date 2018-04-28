e08ef7805c5a4dfd0bdf237cf0ac91b56026f2f6
hai-lin <xianghailin@learnta.com>
2018-04-28 14:55:22 +0800

    eureka server/client demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spring-cloud-demo
  Spring Cloud Demo Application
+
+# Environment
+ Intellij    : 2018,
+ JDK         : 10,
+ gradle      : 4.7,
+ SpringBoot  : 2.0.1.RELEASE
---
+++ eureka-client/src/main/java/com/spring/cloud/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,48 @@
+package com.spring.cloud.eureka.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by xiang on 2018/4/24.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Value("${server.port}")
+    private Integer port;
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @RequestMapping("/getInstance")
+    public String getInstance() {
+        return "instance = " + applicationName + ",port = " + port;
+    }
+}
