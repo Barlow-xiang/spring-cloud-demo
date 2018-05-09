384ab8b273c4ad8ba3124f955906d69ef00f0d24
hai-lin <xianghailin@learnta.com>
2018-05-09 13:37:34 +0800

    zipkin server demo

--- eureka-client/src/main/java/com/spring/cloud/eureka/client/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/spring/cloud/eureka/client/EurekaClientApplication.java
@@ -1,12 +1,12 @@
 package com.spring.cloud.eureka.client;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,27 +23,5 @@ public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
-    }
-}
-
-@RestController
-class ServiceInstanceRestController {
-
-    @Value("${server.port}")
-    private Integer port;
-    @Value("${spring.application.name}")
-    private String applicationName;
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
-
-    @RequestMapping("/getInstance")
-    public String getInstance() {
-        return "instance = " + applicationName + ",port = " + port;
     }
 }
